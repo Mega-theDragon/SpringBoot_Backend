73070fa3de736fce4bf10c125f2dc37fe3416c42
Mega the Dragon <megathedragon79@gmail.com>
2025-01-28 11:50:43 +0100

    Configure the project to work with PostgreSQL

--- src/main/java/com/practice/SpringBootBackend/model/Person.java
+++ src/main/java/com/practice/SpringBootBackend/model/Person.java
@@ -1,14 +1,18 @@
 package com.practice.SpringBootBackend.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Person {
 
+    @Id
     private int id;
     private String name;
     private int age;
