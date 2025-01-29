8647e5a6a5337da28bf2555fe8b9ca3147bb6aee
Mega the Dragon <megathedragon79@gmail.com>
2025-01-29 10:25:37 +0100

    Add CSRF token request endpoint

--- src/main/java/com/practice/SpringBootBackend/HomeController.java
+++ src/main/java/com/practice/SpringBootBackend/HomeController.java
@@ -1,7 +1,12 @@
 package com.practice.SpringBootBackend;
 
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 public class HomeController {
@@ -9,6 +14,16 @@ public class HomeController {
     @RequestMapping("/")
     public String home(){
         return "Hello there";
+    }
+
+    @PostMapping("/")
+    public String postTest(){
+        return "Posted";
+    }
+
+    @GetMapping("/csrf")
+    public CsrfToken csrfToken(HttpServletRequest request){
+        return (CsrfToken) request.getAttribute("_csrf");
     }
 
 }
